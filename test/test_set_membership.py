9e3ea39292723e08eee26afbdb9f772aaa712d03
rewalczak <rewalczak@gmail.com>
2020-03-04 15:13:16 -0600

    Add files via upload

--- test/test_set_membership.py
+++ test/test_set_membership.py
@@ -6,13 +6,11 @@ from more_fun_with_collections import set_membership
 class SetTest(unittest.TestCase):
     def test_in_set_true(self):
         set = {1, 2, 3, 4, 5}
-        expected_result = True
-        self.assertEqual(expected_result, set_membership.in_set(set, 3), True)
+        self.assertEqual(True, set_membership.in_set(set, 3), True)
 
     def test_in_set_false(self):
         set = {1, 2, 3, 4, 5}
-        expected_result = False
-        self.assertEqual(expected_result, set_membership.in_set(set, 9), False)
+        self.assertEqual(False, set_membership.in_set(set, 9), False)
 
 
 if __name__ == '__main__':
